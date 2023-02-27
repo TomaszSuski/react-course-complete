6b4559720bbd24d915ec258a300669933be1b588
Tomasz Suski <tomasz.suski@orba.co>
2023-02-27 09:29:53 +0100

    task 4, added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,7 @@ const ExpenseForm = ({ setExpenses }) => {
     enteredAmount: "",
     enteredDate: "",
   });
+  const [formVisible, setFormVisible] = useState(false);
 
   const titleChangeHandler = (event) => {
     setUserInput((prevState) => {
@@ -45,43 +46,61 @@ const ExpenseForm = ({ setExpenses }) => {
       enteredDate: "",
     });
   };
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={userInput.enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={userInput.enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={userInput.enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
+
+  const toggleVisible = () => {
+    let isFormVisible = !formVisible;
+    console.log(formVisible);
+    console.log(isFormVisible);
+    setFormVisible(isFormVisible);
+  };
+  if (formVisible === false) {
+    return (
       <div className="new-expense__actions">
-        <button type="submit">Submit</button>
+        <button onClick={toggleVisible}>Add new expense</button>
       </div>
-    </form>
-  );
+    );
+  } else {
+    return (
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={userInput.enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={userInput.enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={userInput.enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
+        </div>
+        <div>
+          <button onClick={toggleVisible}>Cancel</button>
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
 };
 
 export default ExpenseForm;
